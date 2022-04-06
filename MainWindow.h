edb565ffc52ec14c945a5422f7ae24df6c633231
jsaunders25 <86677482+jsaunders25@users.noreply.github.com>
2022-04-06 19:53:59 -0400

    Scrabble score functionality

--- MainWindow.h
+++ MainWindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMap>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -20,11 +21,17 @@ private:
     Ui::MainWindow *ui;
     WordleWinner *wordle_winner;
     QStringListModel *model;
+    QMap<QChar, int> scrabble_scores;
+    QStringList ignored_words;
+
+    int calculateScrabbleScore(QString word);
+    void populateScoreMap();
 
 private slots:
     void receiveGuess(QString word, int hints[5]);
     void receiveRemainingWords(QSet<QString> words);
     void on_pushButton_reset_clicked();
     void on_listView_remaining_pressed(const QModelIndex &index);
+    void on_pushButton_another_clicked();
 };
 #endif // MAINWINDOW_H
